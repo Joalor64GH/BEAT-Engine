fceb67809bb1f036dc33636128d84488129021a7
Luisinho01 <91190561+Luisinhi010@users.noreply.github.com>
2022-01-25 19:00:48 -0300

    test

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -74,6 +74,7 @@ class PlayState extends MusicBeatState
 		['Perfect!!', 1] // The value on this one isn't used actually, since Perfect is always "1"
 	];
 
+	// test
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
 	public var modchartSprites:Map<String, ModchartSprite> = new Map<String, ModchartSprite>();
 	public var modchartTimers:Map<String, FlxTimer> = new Map<String, FlxTimer>();
