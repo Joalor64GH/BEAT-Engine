5d18dffca23a28c18caf795903aa1c30c255c8db
Luisinho01 <91190561+Luisinhi010@users.noreply.github.com>
2022-03-13 15:39:27 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -42,8 +42,8 @@ class MainMenuState extends MusicBeatState
 {
 	public static var fridayVersion:String = '0.2.7-Git';
 	public static var psychEngineVersion:String = '0.5.1';
-	public static var beatEngineVersion:String = '0.0.2'; // this is used for Discord RPC
-	public static var beatEngineGit:String = '0.0.4-Git';
+	public static var beatEngineVersion:String = '0.0.4 Custom'; // this is used for Discord RPC
+	public static var beatEngineGit:String = 'Git-Custom-Build';
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
